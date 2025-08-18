2d033bceaf5e9a4025823eaf64c9292b733639e3
unknown <dagimt369@gmail.com>
2025-08-18 17:23:11 +0200

    single product fixed

--- app/vendor/manageProduct/[id]/page.tsx
+++ app/vendor/manageProduct/[id]/page.tsx
@@ -1,18 +1,7 @@
 
 import SingleProductPage from "./SingleProductPage";
 
-export const revalidate = 3600;
-
-export async function generateStaticParams() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/allProducts`,
-    { next: { revalidate: 3600 } }
-  );
-  const data = await res.json();
-  return data.products.map((p: any) => ({
-    id: p._id.toString(),
-  }));
-}
+export const dynamic = "force-dynamic";
 
 export default async function Page({
   params,
