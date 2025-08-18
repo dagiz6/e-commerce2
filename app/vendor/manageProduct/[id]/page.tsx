24ee11c257dcca4456e521d0ba7a3ba9630ce9bd
unknown <dagimt369@gmail.com>
2025-08-18 09:29:13 +0200

    single product fixed

--- app/vendor/manageProduct/[id]/page.tsx
+++ app/vendor/manageProduct/[id]/page.tsx
@@ -1,12 +1,23 @@
-// app/vendor/manageProduct/[id]/page.tsx
+
 import SingleProductPage from "./SingleProductPage";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 3600;
 
-type Props = {
-  params: { id: string };
-};
+export async function generateStaticParams() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/allProducts`
+  );
+  const data = await res.json();
+  return data.products.map((p: any) => ({
+    id: p._id.toString(),
+  }));
+}
 
-export default function ManageProductPage({ params }: Props) {
-  return <SingleProductPage productId={params.id} />;
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const resolvedParams = await params; // Await the params promise
+  return <SingleProductPage productId={resolvedParams.id} />;
 }
